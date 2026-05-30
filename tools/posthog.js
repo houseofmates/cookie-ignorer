1047b68529db24992871031c9a862fe0d477368c
houseofmates <house@houseofmates.dev>
2026-05-30 12:57:53 -0700

    auto: 2026-05-30 12:57:53

---
+++ tools/posthog.js
@@ -0,0 +1,11 @@
+import { PostHog } from "posthog-node";
+import { hostname } from "os";
+
+export const posthog = new PostHog(process.env.POSTHOG_API_KEY, {
+  host: process.env.POSTHOG_HOST,
+  flushAt: 1,
+  flushInterval: 0,
+  enableExceptionAutocapture: true,
+});
+
+export const distinctId = hostname();
